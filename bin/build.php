325b582027a7f0c246be762b613f28f4d5f70e5d
Nathan Van Overloop <nathan.van.overloop@centralnic.com>
2020-06-12 00:08:07 +0200

    centralnic/issues#4052: adapt with new list

--- bin/build.php
+++ bin/build.php
@@ -1,30 +1,37 @@
 <?php
-
-// generate an XML registry file from plain text files using a template
-
+chdir("Net_IDNA2");
 require('Net/IDNA2.php');
-
 $idn = new Net_IDNA2;
-
-$doc = DOMDocument::load('registry.xml.in');
+$doc= new DOMDocument();
+chdir("../..");
+$doc->load('registry.xml.in');
 
 $registries = $doc->getElementsByTagName('registry');
 for ($i = 0 ; $i < $registries->length ; $i++) {
-	$registry = $registries->item($i);
-	$file = $registry->getAttribute('id').'.txt';
-	if (!file_exists($file)) continue;
+    $registry = $registries->item($i);
+    $file = $registry->getAttribute('id').'.txt';
+    if (!file_exists($file)) {
+        continue;
+    }
 
-	$lines = array_map('trim', file($file));
-	foreach ($lines as $line) {
-		$line = preg_replace('/\s*#.*/', '', $line);
-		if (empty($line)) continue;
-		$record = $doc->createElement('record');
-		$name = $doc->createElement('name', $line);
-		$label = $doc->createElement('label', $idn->encode($line));
-		$record->appendChild($name);
-		$record->appendChild($label);
-		$registry->appendChild($record);
-	}
+    $lines = array_map('trim', file($file));
+    foreach ($lines as $line) {
+        $line = preg_replace('/\s*#.*/', '', $line);
+        if (empty($line)) {
+            continue;
+        }
+        $record = $doc->createElement('record');
+        $name = $doc->createElement('name', $line);
+        if (preg_match('/^xn--/', $line)) {
+            $label = $doc->createElement('label', $line);
+        }
+        else {
+            $label= $doc->createElement('label',$idn->encode($line));
+        }
+        $record->appendChild($name);
+        $record->appendChild($label);
+        $registry->appendChild($record);
+    }
 }
 
 print $doc->saveXML();
